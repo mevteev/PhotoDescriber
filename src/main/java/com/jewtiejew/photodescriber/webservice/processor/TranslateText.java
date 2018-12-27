5f927ac0c39d4f50e59b4d546182c7f2355cbb7d
Mikhail Evteev <mevteev@gmail.com>
2018-12-27 15:34:36 +0300

    remove duplicated words. fixes #9

--- src/main/java/com/jewtiejew/photodescriber/webservice/processor/TranslateText.java
+++ src/main/java/com/jewtiejew/photodescriber/webservice/processor/TranslateText.java
@@ -1,6 +1,7 @@
 package com.jewtiejew.photodescriber.webservice.processor;
 
 import com.jewtiejew.photodescriber.webservice.service.aws.Translator;
+import com.jewtiejew.photodescriber.webservice.service.text.TextUtils;
 import com.jewtiejew.photodescriber.webservice.vo.Response;
 import com.jewtiejew.photodescriber.webservice.vo.TranslateRequest;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,6 @@ public class TranslateText implements Processor<TranslateRequest, Response> {
     @Override
     public Response process(TranslateRequest request) {
         return new Response(translator.translate(request.getText(), request.getSourceLanguage(),
-                        request.getDestLanguage()));
+                        request.getDestLanguage(), TextUtils::removeDuplicatedWords));
     }
 }
--- src/main/java/com/jewtiejew/photodescriber/webservice/service/aws/Translator.java
+++ src/main/java/com/jewtiejew/photodescriber/webservice/service/aws/Translator.java
@@ -1,6 +1,11 @@
 package com.jewtiejew.photodescriber.webservice.service.aws;
 
+import java.util.function.Function;
+
 public interface Translator {
 
     String translate(String text, String from, String to);
+
+    String translate(String text, String from, String to,
+                     Function<String, String> postProcess);
 }
